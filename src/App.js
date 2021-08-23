6ddfb125cb53e73675950dd3659c7ead6268825c
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-23 21:39:54 +0430

    finish markdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import ReactMarkdown from 'react-markdown'
+
 function App() {
-  return <div className='App'></div>
+  const inputRef = useRef('')
+  const [markdown, setMarkdown] = useState('')
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
+  return (
+    <div className='main'>
+      <section className='left'>
+        <textarea
+          className='input'
+          ref={inputRef}
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+      </section>
+
+      <section className='right'>
+        <div className='display-markdown'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default App
